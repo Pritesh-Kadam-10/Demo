06f6ea2fc8d1ff936eb97ade37e03cec93582a3d
Pritesh <priteshkumar.kadam@meg-nxt.com>
2025-04-29 11:32:30 +0530

    update

--- __init__.py
+++
@@ -1,7 +0,0 @@
-import logging
-import os
-from pathlib import Path 
-
-logging.basicConfig(level=logging.INFO,format = "[%(asctime)s]: %(message)s:")
-project_name = "medical_report_analysis"
-
--- setting.py
+++
@@ -1,3 +0,0 @@
-LOCAL_TEST = False   # if local test = True than pass the pdf file path 
-MODEL_NAME = "gemini-1.5-pro" # LLM model name 
-OCR_MODEL_LANGUAGE = "en" # ocr model language.
